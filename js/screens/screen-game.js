f1573854a8b6f9dcb0718c3c3602ce8733b59bfb
Vyacheslav Oblasov <slava@logax.ru>
2017-11-04 01:58:44 +0500

    Внес рекомендации наставника по module6-task2

--- js/screens/screen-game.js
+++ js/screens/screen-game.js
@@ -24,6 +24,7 @@ class ScreenGame {
   init(state) {
     // текущее состояние игры
     this.state = state;
+    this.timer = new Timer(this.state.time);
     this.renderGameScreen();
   }
 
@@ -33,52 +34,51 @@ class ScreenGame {
       // на любой ответ на последнем игровом экране, любой блок .game__option
       App.showStats(this.state);
     } else {
-      const timer = new Timer(this.state.time);
+
       const screenData = screens[this._gameScreenNum];
+
       // выбираем отображение в зависимости от типа игры
-      switch (screens[this._gameScreenNum].type) {
-        // если нужно выбрать из трех картинок
-        case GameType.THREE:
-          this.view = new GameThreeView(screenData, this.state.userAnswers, this.state);
-          break;
-        default:
-          this.view = new GameView(screenData, this.state.userAnswers, this.state);
-          break;
-      }
+      this.view = (screens[this._gameScreenNum].type === GameType.THREE) ?
+        new GameThreeView(screenData, this.state.userAnswers, this.state) :
+        new GameView(screenData, this.state.userAnswers, this.state);
 
       // добавляем коллбек при ответе пользователя
       this.view.onAnswer = (answersData = null) => {
-        // флаг, правильный ли ответ
-        let correct = false;
         // останавливаем таймер
-        timer.stop();
-        if (answersData) {
+        this.timer.stop();
+        // флаг, правильный ли ответ
+        const correct = (answersData) ?
           // проверяем все ли ответы пользователя верны
-          correct = answersData.every((answer) => {
+          answersData.every((answer) => {
             return isCorrect(answer.img, answer.type, screenData.questions);
-          });
-        }
+          }) : false;
+
         // добавляем ответ
-        this.state = addAnswer(correct, timer.value, this.state);
+        this.state = addAnswer(correct, this.timer.value, this.state);
         this.renderGameScreen(this.state);
       };
 
       // отлавливаем событие клика по кнопке "Назад"
       this.view.onBackButtonClick = () => {
-        // отрисовываем первый экран
-        App.showGreeting();
+        if (confirm(`Вы уверены, что хотите вернуться? Игра будет потеряна`)) { // eslint-disable-line
+          // останавливаем таймер
+          this.timer.stop();
+          // отрисовываем первый экран
+          App.showGreeting();
+        }
       };
       // отрисовываем этот экран
       renderScreen(this.view.getMarkup());
 
       // перерисовываем таймер при его изменении
-      timer.onChange = (time) => {
+      this.timer.onChange = (time) => {
         this.view.setTime(time);
       };
       // запускаем таймер
-      timer.start();
-      // если таймер истек считаем, что пользователь ответил неверно
-      timer.onExpire = () => {
+      this.timer.value = this.state.time;
+      this.timer.start();
+      // если таймер истек, считаем, что пользователь ответил неверно
+      this.timer.onExpire = () => {
         this.view.onAnswer();
       };
     }
